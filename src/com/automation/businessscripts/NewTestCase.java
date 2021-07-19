cec02ae13c60087b1100e5b2e1573291f454ced8
harish <harish@DESKTOP-RV7KF9C>
2021-07-19 12:30:01 +0530

    This is first commit

--- src/com/automation/businessscripts/NewTestCase.java
+++ src/com/automation/businessscripts/NewTestCase.java
@@ -13,35 +13,35 @@ public class NewTestCase {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-         String s = "abc@1234";
+      //   String s = "abc@1234";
           // s.replaceAll("abc@123", 1234);
           //  int num= 1234;
-              char[]  ch =  s.toCharArray();
-              System.out.println(ch);
-                   String[] s2 =    s.split("abc@");
-                      String s3 =       s.replace("abc@1234", "1234");
+           //   char[]  ch =  s.toCharArray();
+            //  System.out.println(ch);
+             //      String[] s2 =    s.split("abc@");
+              //        String s3 =       s.replace("abc@1234", "1234");
                    //System.out.println(s2.toString());
                       
                     //  webpagelinks page  get all links    20 , 10 ,duplicate  unique link 
-                          List<WebElement>   list =      driver.findElement(By.tagName("li");
-                                    for (int i =0;i<list.size();i++) {
+                       //   List<WebElement>   list =      driver.findElement(By.tagName("li");
+                               //     for (int i =0;i<list.size();i++) {
                                     	//Set s3 = new HashSet();
-                                    	  HashSet s3<List<WebElement> = new HashSet();
-                                    	     s.add(list);
+                                    	//  HashSet s3<List<WebElement> = new HashSet();
+                                    	//     s.add(list);
                                     	     // 
-                                    	     @Test
-                                    	     method1
-                                    	     method2
-                                    	      method3
-                                    	      method5
+                                    	 //    @Test
+                                    	 //    method1
+                                    	  //   method2
+                                    	 //     method3
+                                    	   //   method5
                                     	      
-                                    	      class {
-                                    	    	   5 methds
-                                    	    	     3 @Test 
-                                    	    	     normal 
-                                    	    	     local modified 
-                                    	     }
-                                    	     }
+                                    	    //  class {
+                                    	    //	   5 methds
+                                    	    //	     3 @Test 
+                                    	    //	     normal 
+                                    	    //	     local modified 
+                                    	   //  }
+                                    	  //   }
                                     	      
                                     	//list.get(i);
                                     	//Set s = new HashSet();
@@ -53,4 +53,4 @@ public class NewTestCase {
           
 	}
 
-}
+//}
